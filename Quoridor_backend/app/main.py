36e01360c9aa5ddf1325c665590d3f35c45349e4
melissa.chikbouni <melissa.chikbouni@gmail.com>
2025-05-07 13:14:38 +0200

    Sauvegarde de mes modifs locales

--- Quoridor_backend/app/main.py
+++ Quoridor_backend/app/main.py
@@ -1,7 +1,10 @@
-
 import sys
 from fastapi import FastAPI
 import logging
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.responses import HTMLResponse
+from fastapi import Request
 from app.controllers import game_controller, websocket_controller
 
 
@@ -11,7 +14,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
         logging.StreamHandler(sys.stdout)  # Affiche dans la console
-        # apre je vais voir si j'ajoute un FileHandler pour écrire dans un fichier logging.FileHandler("quoridor.log") 
+        # Après, tu peux ajouter un FileHandler pour écrire dans un fichier : logging.FileHandler("quoridor.log") 
     ]
 )
 
@@ -20,17 +23,30 @@ logger = logging.getLogger("quoridor")
 
 app = FastAPI(title="Quoridor Game API")
 
-@app.get("/")
+# Route pour l'API
+@app.get("/api")
 def read_root():
     return {"message": "Bienvenue sur l'API Quoridor. Pour la documentation, accédez à /docs"}
 
+# Configurer les fichiers statiques
+app.mount("/static", StaticFiles(directory="Quoridor_front/static"), name="static")
 
+# Configurer les templates
+templates = Jinja2Templates(directory="Quoridor_front/templates")
+
+# Route pour servir le front
+@app.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+#-------------------
+
+# Inclure les routes pour le jeu et le websocket
 app.include_router(game_controller.router, prefix="/api")
 app.include_router(websocket_controller.router)  # Accessible via /ws
 
-# démarrage de l'application
+# Démarrage de l'application
 logger.info("Application Quoridor démarrée")
-
 
 # Pour lancer le serveur, exécutez par exemple :
 # python uvicorn app.main:app --reload
